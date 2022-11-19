1a4256e4d0e3d987ad8274b2bc80b45ac6359231
Tim <48436110+Baqiwaqi@users.noreply.github.com>
2022-11-19 22:29:52 +0100

    Fetched notion database

--- components/grid-item.js
+++ components/grid-item.js
@@ -32,3 +32,35 @@ export const RecipesGridItem = ({ children, post, id, title, thumbnail }) => {
       </Box >
    );
 };
+
+export const NotionGridItem = ({ children, post, id, title, thumbnail }) => {
+   return (
+      <Box w="100%" maxHeight={300} borderRadius="lg" shadow="md" p={3} >
+         <NextLink
+            as={`/posts/${id}`}
+            href={`/posts/[slug]`}
+         >
+            <LinkBox cursor="pointer">
+               <Image
+                  src={thumbnail}
+                  alt={title}
+                  className="grid-item-thumbnail"
+                  placeholder="blur"
+                  width="786px"
+                  height="432px"
+                  objectFit="cover"
+                  blurDataURL="https://developers.google.com/static/maps/documentation/streetview/images/error-image-generic.png"
+                  loading="lazy"
+               />
+               <LinkOverlay href={`/works/${id}`}>
+                  <Text mt={2} fontSize={20}>
+                     {title}
+                  </Text>
+               </LinkOverlay>
+               <Text fontSize={14} noOfLines={2} >{children}</Text>
+            </LinkBox>
+         </NextLink>
+      </Box >
+   );
+};
+
--- components/navbar.js
+++ components/navbar.js
@@ -1,79 +1,79 @@
 import NextLink from "next/link";
 import {
-  Container,
-  Box,
-  Link,
-  Stack,
-  Heading,
-  Flex,
-  Menu,
-  MenuItem,
-  MenuList,
-  MenuButton,
-  IconButton,
-  useColorModeValue,
+   Container,
+   Box,
+   Link,
+   Stack,
+   Heading,
+   Flex,
+   Menu,
+   MenuItem,
+   MenuList,
+   MenuButton,
+   IconButton,
+   useColorModeValue,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons/";
 import Logo from "../components/logo";
 import ThemeToggleButton from "./theme-toggle-button";
 
 const LinkItem = ({ href, path, children }) => {
-  const active = path === href;
-  const inactiveColor = useColorModeValue("gray200", "whiteAlpha.900");
+   const active = path === href;
+   const inactiveColor = useColorModeValue("gray200", "whiteAlpha.900");
 
-  return (
-    <NextLink href={href} passHref={true}>
-      <Link
-        p={2}
-        bg={active ? "glassTeal" : undefined}
-        color={active ? "#202023" : inactiveColor}
-      >
-        {children}
-      </Link>
-    </NextLink>
-  );
+   return (
+      <NextLink href={href} passHref={true}>
+         <Link
+            p={2}
+            bg={active ? "glassTeal" : undefined}
+            color={active ? "#202023" : inactiveColor}
+         >
+            {children}
+         </Link>
+      </NextLink>
+   );
 };
 
 const NavBar = (props) => {
-  const { path } = props;
-  return (
-    <Box
-      position="fixed"
-      as="nav"
-      w="100%"
-      bg={useColorModeValue("#ffffff40", "#20202380")}
-      style={{ backdropFilter: "blur(10px" }}
-      zIndex={1}
-      {...props}
-    >
-      <Container
-        display="flex"
-        p={2}
-        maxW="container.md"
-        wrap="wrap"
-        align="center"
-        jusitfy="space-between"
+   const { path } = props;
+   return (
+      <Box
+         position="fixed"
+         as="nav"
+         w="100%"
+         bg={useColorModeValue("#ffffff40", "#20202380")}
+         style={{ backdropFilter: "blur(10px" }}
+         zIndex={1}
+         {...props}
       >
-        <Flex align="center" mr={5} mb={2}>
-          <Heading as="h1" size="lg" letterSpacing={"tight"}>
-            <Logo />
-          </Heading>
-        </Flex>
+         <Container
+            display="flex"
+            p={2}
+            maxW="container.md"
+            wrap="wrap"
+            align="center"
+            jusitfy="space-between"
+         >
+            <Flex align="center" mr={5} mb={2}>
+               <Heading as="h1" size="lg" letterSpacing={"tight"}>
+                  <Logo />
+               </Heading>
+            </Flex>
 
-        {/* <Stack
-          direction={{ base: "column", md: "row" }}
-          display={{ base: "none", md: "flex" }}
-          width={{ base: "full", md: "auto" }}
-          alignItems="center"
-          flexGrow={1}
-          mt={{ base: 4, nmd: 0 }}
-        >
-          <LinkItem href="/" path={path}>
-            Recepten
-          </LinkItem>
-        </Stack> */}
+            <Stack
+               direction={{ base: "column", md: "row" }}
+               display={{ base: "none", md: "flex" }}
+               width={{ base: "full", md: "auto" }}
+               alignItems="center"
+               flexGrow={1}
+               mt={{ base: 4, nmd: 0 }}
+            >
+               <LinkItem href="/notion" path={path}>
+                  Notion
+               </LinkItem>
+            </Stack>
 
-        {/* <Box flex={1} align="right">
+            {/* <Box flex={1} align="right">
           <ThemeToggleButton />
           <Box ml={2} display={{ base: "inline-block", md: "none" }}>
             <Menu>
@@ -92,9 +92,9 @@ const NavBar = (props) => {
             </Menu>
           </Box>
         </Box> */}
-      </Container>
-    </Box>
-  );
+         </Container>
+      </Box>
+   );
 };
 
 export default NavBar;
